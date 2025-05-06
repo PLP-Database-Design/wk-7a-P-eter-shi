1263333fa83d0bc83b81da441f8846b46750c6b9
P-eter-shi <pwanjala404@gmail.com>
2025-05-06 08:16:24 +0300

    week 7

--- answers.sql
+++ answers.sql
@@ -12,12 +12,14 @@ CREATE TABLE products(
     FOREIGN KEY (OrderID) REFERENCES orders(OrderID) --foregn key constraint
 ); -- removes the using of ,more items in one row
 
-INSERT INTO orders VALUES --added values to the orders table
+INSERT INTO orders(OrderID, CustomerName) --added values to the orders table
+VALUES 
 (101, 'John Doe')
 (102, 'Jane Smith'),
 (103, 'Emily Clark');
 
-INSERT INTO products VALUES --added values to the products table
+INSERT INTO products(OrderID, Product)--added values to the products table
+ VALUES 
 (101, 'Laptop'),
 (101, 'Mouse'),
 (102, 'Tablet'),
@@ -40,12 +42,14 @@ CREATE TABLE order_products(
     FOREIGN KEY (OrderID) REFERENCES orders(OrderID) --foreign key constraint
 );--this ensures removal of partial dependency
 
-INSERT INTO orders_t VALUES --added values to the orders_t table
+INSERT INTO orders_t (OrderID, CustomerName)--added values to the orders_t table
+VALUES 
 (101, 'John Doe')
 (102, 'Jane Smith'),
 (103, 'Emily Clark');
 
-INSERT INTO 0rder_products VALUES --added values to the order_products table
+INSERT INTO 0rder_products (OrderID, Product, Quantity)--added values to the order_products table
+VALUES 
 (101, 'Laptop', 2),
 (101, 'Mouse', 1),
 (102, 'Tablet', 3),
